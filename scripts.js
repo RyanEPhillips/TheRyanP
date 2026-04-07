c27f370a4e94266643d93d5f7630e75d69e758bc
Ryan <85RyanP@gmail.com>
2026-04-07 09:35:43 -0400

    lil updates

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 let total = 0;
 
 function addOne(button) {
-    total += 1;
-    document.getElementById("total").innerHTML = total;
-    button.disabled = true;
+    if (button.classList.contains("seen")) {
+        // Uncheck
+        total--;
+        button.classList.remove("seen");
+        button.innerText = "Seen it";
+    } else {
+        // Check
+        total++;
+        button.classList.add("seen");
+        button.innerText = "Unseen";
+    }
 
+    document.getElementById("total").innerHTML = total;
 }
