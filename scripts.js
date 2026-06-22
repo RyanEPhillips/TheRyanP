011c867687fb87c7d5a97f367e7a71fa6a4e6e20
Ryan <85RyanP@gmail.com>
2026-06-22 15:19:48 -0400

    working on navbar

--- scripts.js
+++ scripts.js
@@ -101,3 +101,18 @@ soundclips.forEach(clip => {
         audio.play();
     });
 });
+
+// nav bar
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      top: target.offsetTop - 80, 
+      behavior: "smooth"
+    });
+  });
+});
